def43b6d626dc032af5a2a5af23144cd2d2b978d
ahmadumar10 <ahmad.umar@mitre10.co.nz>
2022-05-05 04:39:38 +1200

    added alb

--- simple-setup/main.tf
+++ simple-setup/main.tf
@@ -31,6 +31,7 @@ resource "aws_vpc" "terraform-vpc" {
 resource "aws_subnet" "subnet1" {
   vpc_id = "${aws_vpc.terraform-vpc.id}"
   cidr_block = "10.3.0.0/20"
+  availability_zone = "ap-southeast-2a"
 
   depends_on = [
       aws_vpc.terraform-vpc
@@ -44,6 +45,7 @@ resource "aws_subnet" "subnet1" {
 resource "aws_subnet" "subnet2" {
   vpc_id = "${aws_vpc.terraform-vpc.id}"
   cidr_block = "10.3.16.0/20"
+  availability_zone = "ap-southeast-2b"
 
   depends_on = [
       aws_vpc.terraform-vpc
@@ -51,6 +53,68 @@ resource "aws_subnet" "subnet2" {
 
   tags = {
     "Name" = "subnet2"
+  }
+}
+
+resource "aws_network_acl" "terraform-nacl" {
+  vpc_id     = aws_vpc.terraform-vpc.id
+  subnet_ids = [aws_subnet.subnet1.id,aws_subnet.subnet2.id]
+
+  # Ingress rules
+  /*
+  # Allow all local traffic
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.terraform-vpc.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }*/
+
+  # Allow HTTP web traffic from anywhere
+  ingress {
+    protocol   = 6
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # Allow HTTPS web traffic from anywhere
+  ingress {
+    protocol   = 6
+    rule_no    = 110
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  # Allow HTTPS web traffic from anywhere
+  ingress {
+    protocol   = 6
+    rule_no    = 120
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  # Egress rules
+  # Allow all ports, protocols, and IPs outbound
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "terraform-nacl"
   }
 }
 
@@ -113,16 +177,97 @@ resource "aws_lb" "web-alb" {
   enable_http2       = false
   enable_deletion_protection = false
 
+  tags = {
+    Name = "web-alb"
+  }
+
   depends_on = [
       aws_security_group.allow_web,
       aws_subnet.subnet1,
       aws_subnet.subnet2
     ]
+}
+
+resource "aws_lb_listener" "web-alb" {
+  load_balancer_arn = aws_lb.web-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web-alb.arn
+  }
+
+  depends_on = [
+    aws_lb.web-alb,
+    aws_lb_target_group.web-alb
+  ]
+}
+
+resource "aws_lb_target_group" "web-alb" {
+  name     = "web-alb-target"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.terraform-vpc.id
+
+  health_check {
+    port     = 80
+    protocol = "HTTP"
+    timeout  = 5
+    interval = 10
+  }
+
+  depends_on = [
+    aws_vpc.terraform-vpc
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "web-alb" {
+  #count            = length(aws_instance.terraform-instance)
+  target_group_arn = aws_lb_target_group.web-alb.arn
+  target_id        = aws_instance.terraform-instance.id
+  #target_id        = aws_instance.terraform-instance[count.index].id
+  port             = 80
+
+  depends_on = [
+    aws_lb_target_group.web-alb,
+    aws_instance.terraform-instance
+  ]
+}
+
+## ami-0c6120f461d6b39e9 (Amazon Linux 2 AMI)
+## instance type is t2.micro (free tier)
+
+resource "aws_instance" "terraform-instance" {
+  ami           = "ami-0c6120f461d6b39e9"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
+  #security_groups   = [aws_security_group.allow_web.name]
+  subnet_id = aws_subnet.subnet1.id
+  key_name = "demo-key-pair"
+  depends_on = [
+    aws_vpc.terraform-vpc,
+    aws_security_group.allow_web
+  ]
+
+  user_data         = <<-EOF
+                #! /bin/bash
+                sudo yum update
+                sudo yum install -y httpd
+                sudo systemctl start httpd
+                sudo systemctl enable httpd
+                echo "
+<h1>Deployed via Terraform</h1>
+
+" | sudo tee /var/www/html/index.html
+        EOF
 
   tags = {
-    Name = "web-alb"
+    Name = "terraform-instance"
   }
 }
+
+
 
 /*
  #Route table for Public Subnet's
@@ -247,7 +392,7 @@ resource "aws_network_acl" "demo-nacl" {
 ## ami-0c6120f461d6b39e9 (Amazon Linux 2 AMI)
 ## instance type is t2.micro (free tier)
 
-resource "aws_instance" "demo-instance" {
+resource "aws_instance" "terraform-instance" {
   ami           = "ami-0c6120f461d6b39e9"
   instance_type = "t2.micro"
   key_name          = "demo-key-pair"
@@ -265,14 +410,14 @@ resource "aws_instance" "demo-instance" {
         EOF
 
   tags = {
-    Name = "demo-instance"
+    Name = "terraform-instance"
   }
 }
 /*
 
 resource "aws_lb_target_group_attachment" "demo-alb-attach" {
   target_group_arn = "${aws_alb_target_group.demo-group.arn}"
-  target_id        = aws_instance.demo-instance.id
+  target_id        = aws_instance.terraform-instance.id
   port             = 80
 }
 
--- simple-setup/provider.tf
+++ simple-setup/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = "AKIAS4OOWAGPXYNAF2LA"
-    secret_key = "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"
+    access_key = "AKIAS4OOWAGPQ2IYCXEP"
+    secret_key = "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"
     region = "ap-southeast-2"
 }
